9f3ce6534ff2fccfb0fc4f6ef281fb6a5f210bae
Rajanikant deshmukh <rajanikantdeshmukh@gmail.com>
2015-09-03 23:32:40 +0530

    Add solution for Lesson 1.

---
+++ Lessons/1 Time Complexity/PermMissingElem.java
@@ -0,0 +1,68 @@
+/*
+
+    ## Problem Statement :
+
+    A zero-indexed array A consisting of N different integers is given. The 
+    array contains integers in the range [1..(N + 1)], which means that exactly 
+    one element is missing.
+
+    Your goal is to find that missing element.
+
+    Write a function:
+
+        class Solution { public int solution(int[] A); }
+
+    that, given a zero-indexed array A, returns the value of the missing element.
+
+    For example, given array A such that:
+
+      A[0] = 2
+      A[1] = 3
+      A[2] = 1
+      A[3] = 5
+
+    the function should return 4, as it is the missing element.
+
+    Assume that:
+
+            N is an integer within the range [0..100,000];
+            the elements of A are all distinct;
+            each element of array A is an integer within the range [1..(N + 1)].
+
+    Complexity:
+
+            expected worst-case time complexity is O(N);
+            expected worst-case space complexity is O(1), beyond input storage 
+            (not counting the storage required for input arguments).
+
+    Elements of input arrays can be modified.
+
+*/
+
+/*
+    Correctness : 100% 
+    Performance : 60%
+
+    Wrong answer for large numbers or long ranges 
+    int cannot accumulate sum of numbers given
+*/
+
+class PermMissingElem {
+
+    
+    public static int solution(int[] A) {
+        
+        final int N = A.length;
+		
+		// Sum 1->N = n(n+1)/2
+		int actualSum = N*(N+1)/2;
+		
+		int arraySum = 0;
+		for (int i=0;i<A.length;i++)
+			arraySum+=A[i];
+			
+		int givenSum = arraySum - (N+1);
+		
+		return actualSum - givenSum;
+    }
+}
